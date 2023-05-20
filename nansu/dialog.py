24cb65c865bcf1c9f96c44fa7123168ef34ed5b6
Curtis McQuarrie <42049552+CurtisMcQuarrie@users.noreply.github.com>
2023-05-20 15:31:20 -0500

    Revert "Transactions Update (#8)"
    This reverts commit 18a7dc245e489c39536d83b4386bcbec7b7b2fd6.

--- nansu/dialog.py
+++ nansu/dialog.py
@@ -6,11 +6,9 @@ from PyQt5.QtWidgets import (
     QLineEdit,
     QDateTimeEdit,
     QMessageBox,
-    QComboBox,
 )
 from PyQt5.QtGui import QIntValidator
 from PyQt5.QtCore import QDateTime, Qt
-from .model import PaymentFrequency
 
 
 class AddAccountDialog(QDialog):
@@ -20,6 +18,7 @@ class AddAccountDialog(QDialog):
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
         self.data = None
+
         self.setupUI()
 
     def setupUI(self):
@@ -46,7 +45,7 @@ class AddAccountDialog(QDialog):
         """
         accept data provided through dialog
         """
-        self.data = {}
+        self.data = []
         if not self.name_field.text():
             QMessageBox.critical(
                 self,
@@ -56,7 +55,7 @@ class AddAccountDialog(QDialog):
             self.data = None  # reset data
             return
 
-        self.data["Name"] = self.name_field.text()
+        self.data.append(self.name_field.text())
 
         if not self.data:
             return
@@ -65,13 +64,13 @@ class AddAccountDialog(QDialog):
 
 
 class AddTransactionDialog(QDialog):
-    def __init__(self, account_id, parent=None):
+    def __init__(self, parent=None):
         super().__init__(parent=parent)
-        self.account_id = account_id
         self.setWindowTitle("Nansu - Add Transaction")
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
         self.data = None
+
         self.setupUI()
 
     def setupUI(self):
@@ -113,94 +112,28 @@ class AddTransactionDialog(QDialog):
 
         if not self.data:
             return
-        else:
-            self.data["Account"] = self.account_id
 
         super().accept()
 
 
-class AddPaymentDialog(QDialog):
-    def __init__(self, account_id, parent=None):
-        super().__init__(parent=parent)
-        self.account_id = account_id
-        self.setWindowTitle("Nansu - Add Payment")
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-        self.data = None
-        self.setupUI()
+    # def accept(self):
+    #     """
+    #     accept data provided through dialog
+    #     """
+    #     self.data = []
+    #     for field in (self.amount_field, self.date_field, self.description_field):
+    #         if not field.text():
+    #             QMessageBox.critical(
+    #                 self,
+    #                 "Error!",
+    #                 f"You must provide a transaction's {field.objectName()}",
+    #             )
+    #             self.data = None  # reset data
+    #             return
 
-    def setupUI(self):
-        """
-        setup the add payment dialog's GUI
-        """
-        self.form_fields = list()
+    #         self.data.append(field.text())
 
-        self.amount_field = QLineEdit()
-        self.amount_field.setValidator(QIntValidator())
-        self.amount_field.setObjectName("Amount")
-        self.form_fields.append(self.amount_field)
+    #     if not self.data:
+    #         return
 
-        self.start_date_field = QDateTimeEdit(calendarPopup=True)
-        self.start_date_field.setDateTime(QDateTime.currentDateTime())
-        self.start_date_field.setObjectName("StartDate")
-        self.form_fields.append(self.start_date_field)
-
-        self.end_date_field = QDateTimeEdit(calendarPopup=True)
-        self.end_date_field.setDateTime(QDateTime.currentDateTime())
-        self.end_date_field.setObjectName("EndDate")
-        self.form_fields.append(self.end_date_field)
-
-        self.frequency_field = QComboBox()
-        for freq in PaymentFrequency:
-            self.frequency_field.addItem(freq.value)
-        self.frequency_field.setObjectName("Frequency")
-        self.form_fields.append(self.frequency_field)
-
-        self.description_field = QLineEdit()
-        self.description_field.setObjectName("Description")
-        self.form_fields.append(self.description_field)
-
-        formLayout = QFormLayout()
-        formLayout.addRow("Amount:", self.amount_field)
-        formLayout.addRow("Start Date:", self.start_date_field)
-        formLayout.addRow("End Date:", self.end_date_field)
-        formLayout.addRow("Frequency:", self.frequency_field)
-        formLayout.addRow("Description:", self.description_field)
-        self.layout.addLayout(formLayout)
-
-        self.buttons_box = QDialogButtonBox(self)
-        self.buttons_box.setOrientation(Qt.Horizontal)
-        self.buttons_box.setStandardButtons(
-            QDialogButtonBox.Cancel | QDialogButtonBox.Save
-        )
-        self.buttons_box.accepted.connect(self.accept)
-        self.buttons_box.rejected.connect(self.reject)
-        self.layout.addWidget(self.buttons_box)
-
-    def accept(self):
-        """
-        accept data provided through dialog
-        """
-        self.data = {}
-        for field in self.form_fields:
-            if isinstance(field, QComboBox):
-                text = field.currentText()
-            else:
-                text = field.text()
-            if not text:
-                QMessageBox.critical(
-                    self,
-                    "Error!",
-                    f"You must provide a payment's {field.objectName()}",
-                )
-                self.data = None  # reset data
-                return
-
-            self.data[field.objectName()] = text
-
-        if not self.data:
-            return
-        else:
-            self.data["Account"] = self.account_id
-
-        super().accept()
+    #     super().accept()
--- nansu/model.py
+++ nansu/model.py
@@ -1,187 +1,258 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtSql import (
-    QSqlTableModel, 
-    QSqlRelationalTableModel,
-    QSqlRelation,
-    QSqlQuery,
-)
+from PyQt5.QtSql import QSqlTableModel, QSqlRelationalTableModel, QSqlRelation
 from enum import Enum
-from datetime import timedelta
 
 
-class TableType(Enum):
-    NonRelational = 0
-    Relational = 1
+# class TransactionField(Enum):
+#     Id = 0
+#     Amount = 1
+#     Unit = 2
+#     PaidDate = 3
+#     DueDate = 4
+#     Payment = 5
 
 
-class PaymentFrequency(Enum):
-    Daily = "Daily"
-    Weekly = "Weekly"
-    BiWeekly = "Bi-Weekly"
-    Monthly = "Monthly"
-    BiMonthly = "Bi-Monthly"
-    Annually = "Annually"
-    SemiAnnually = "Semi-Annually"
+# class TransactionsModel:
+#     def __init__(self):
+#         self.model = self._createModel()
+
+#     @staticmethod
+#     def _createModel():
+#         """
+#         create and set up model
+#         """
+#         tableModel = QSqlRelationalTableModel()
+#         tableModel.setTable("transactions")
+#         tableModel.setRelation(
+#             TransactionField.Payment.value, 
+#             QSqlRelation("payments", "id", "description")
+#         )
+#         tableModel.setEditStrategy(QSqlTableModel.OnFieldChange)
+#         tableModel.select()
+#         headers = ("ID", "Amount", "Unit", "PaidDate", "DueDate", "Payment")
+#         for columnIndex, header in enumerate(headers):
+#             tableModel.setHeaderData(columnIndex, Qt.Horizontal, header)
+#         tableModel.setSort(TransactionField.DueDate.value, Qt.AscendingOrder)
+
+#         return tableModel
+
+#     def addTransaction(self, data):
+#         """
+#         add a transaction to the database
+#         """
+#         rows = self.model.rowCount()
+#         self.model.insertRows(rows, 1)
+
+#         self.model.setData(
+#             self.model.index(
+#                 rows, 
+#                 TransactionField.Amount.value
+#             ), data[0]
+#         )
+#         self.model.setData(
+#             self.model.index(
+#                 rows, 
+#                 TransactionField.Date.value
+#             ), data[1]
+#         )
+#         self.model.setData(
+#             self.model.index(
+#                 rows, 
+#                 TransactionField.Description.value
+#             ), data[2]
+#         )
+
+#         self.model.submitAll()
+#         self.model.setSort(TransactionField.DueDate.value, Qt.AscendingOrder)
+#         self.model.select()
+
+#     def deleteTransaction(self, row):
+#         """
+#         remove a transaction from the database
+#         """
+#         self.model.removeRow(row)
+#         self.model.submitAll()
+#         self.model.setSort(TransactionField.DueDate.value, Qt.AscendingOrder)
+#         self.model.select()
+
+#     def clearTransactions(self):
+#         """
+#         remove all transactions from the database
+#         """
+#         self.model.setEditStrategy(QSqlTableModel.OnManualSubmit)
+#         self.model.removeRows(0, self.model.rowCount())
+#         self.model.submitAll()
+#         self.model.setEditStrategy(QSqlTableModel.OnFieldChange)
+#         self.model.setSort(TransactionField.DueDate.value, Qt.AscendingOrder)
+#         self.model.select()
 
 
-class CustomModel:
-    def __init__(self, table_name: str, field_names: list(), table_type: TableType):
-        self.table_name = table_name
-        self.field_names = field_names
-        self.hidden_fields = list()
-        self.table_type = table_type
-        self.sort_field = field_names[0]
-        self.sort_order = Qt.AscendingOrder
+# class PaymentField(Enum):
+#     Id = 0
+#     CreateDate = 1
+#     StartDate = 2
+#     EndDate = 3
+#     Frequency = 4
+#     Description = 5
+#     Account = 6
+
+
+# class Frequency(Enum):
+#     Daily = 0
+#     Weekly = 1
+#     BiWeekly = 2
+#     Monthly = 3
+#     SemiAnnually = 4
+#     Annually = 5
+
+
+# class PaymentModel:
+#     def __init__(self):
+#         self.model = self._createModel()
+
+#     @staticmethod
+#     def createModel():
+#         """
+#         create and set up model
+#         """
+#         tableModel = QSqlRelationalTableModel()
+#         tableModel.setTable("payments")
+#         model.setRelation(
+#             PaymentField.Payment.value,
+#             QSqlRelation("accounts", "id", "name")
+#         )
+#         tableModel.setEditStrategy(QSqlTableModel.OnFieldChange)
+#         tableModel.select()
+#         headers = ("ID", "CreateDate", "StartDate", "EndDate", "Frequency", "Description", "PaymentID")
+#         for columnIndex, header in enumerate(headers):
+#             tableModel.setHeaderData(columnIndex, Qt.Horizontal, header)
+#         tableModel.setSort(PaymentField.StartDate.value, Qt.AscendingOrder)
+
+#         return tableModel
+
+#     def addPayment(self, data):
+#         """
+#         add an account to the database
+#         """
+#         rows = self.model.rowCount()
+#         self.model.insertRows(rows, 1)
+
+#         self.model.setData(
+#             self.model.index(
+#                 rows, 
+#                 PaymentField.StartDate.value
+#             ), data[0]
+#         )
+#         self.model.setData(
+#             self.model.index(
+#                 rows, 
+#                 PaymentField.EndDate.value
+#             ), data[1]
+#         )
+#         self.model.setData(
+#             self.model.index(
+#                 rows, 
+#                 PaymentField.Frequency.value
+#             ), data[2]
+#         )
+#         self.model.setData(
+#             self.model.index(
+#                 rows, 
+#                 PaymentField.Description.value
+#             ), data[3]
+#         )
+#         self.model.setData(
+#             self.model.index(
+#                 rows, 
+#                 PaymentField.Account.value
+#             ), data[4]
+#         )
+
+#         self.model.submitAll()
+#         self.model.setSort(PaymentField.Id.value, Qt.AscendingOrder)
+#         self.model.select()
+
+# def deletePayment(self, row):
+#         """
+#         remove an account from the database
+#         """
+#         self.model.removeRow(row)
+#         self.model.submitAll()
+#         self.model.setSort(AccountField.Id.value, Qt.AscendingOrder)
+#         self.model.select()
+
+#     def clearPayment(self):
+#         """
+#         remove all accounts from the database
+#         """
+#         self.model.setEditStrategy(QSqlTableModel.OnManualSubmit)
+#         self.model.removeRows(0, self.model.rowCount())
+#         self.model.submitAll()
+#         self.model.setEditStrategy(QSqlTableModel.OnFieldChange)
+#         self.model.setSort(AccountField.Id.value, Qt.AscendingOrder)
+#         self.model.select()
+
+
+class AccountField(Enum):
+    Id = 0
+    CreateDate = 1
+    Name = 2
+
+
+class AccountsModel:
+    def __init__(self):
         self.model = self._createModel()
-        self.sort()
 
-    def _createModel(self):
+    @staticmethod
+    def _createModel():
         """
-        create and set up the model
+        create and set up model
         """
-        if self.table_type == TableType.NonRelational.value:
-            tableModel = QSqlTableModel()
-        else:
-            tableModel = QSqlRelationalTableModel()
-            
-        tableModel.setTable(self.table_name)
+        tableModel = QSqlTableModel()
+        tableModel.setTable("accounts")
         tableModel.setEditStrategy(QSqlTableModel.OnFieldChange)
         tableModel.select()
-        headers = (field for field in self.field_names)
+        headers = ("ID", "CreateDate", "Name")
         for columnIndex, header in enumerate(headers):
             tableModel.setHeaderData(columnIndex, Qt.Horizontal, header)
+        tableModel.setSort(AccountField.Id.value, Qt.AscendingOrder)
 
         return tableModel
 
-    def add(self, data):
+    def addAccount(self, data):
         """
-        add an instance to the model
+        add an account to the database
         """
         rows = self.model.rowCount()
         self.model.insertRows(rows, 1)
 
-        for key, value in data.items():
-            if key in self.field_names and key not in self.hidden_fields:
-                # get the index for the table
-                field_index = self.field_names.index(key)
-                self.model.setData(
-                    self.model.index(
-                        rows,
-                        field_index
-                    ), value
-                )
+        self.model.setData(
+            self.model.index(
+                rows, 
+                AccountField.Name.value
+            ), data[0]
+        )
 
         self.model.submitAll()
-        self.sort()
+        self.model.setSort(AccountField.Id.value, Qt.AscendingOrder)
         self.model.select()
 
-    def delete(self, row):
+    def deleteAccount(self, row):
         """
-        delete an instance from the model
+        remove an account from the database
         """
         self.model.removeRow(row)
         self.model.submitAll()
-        self.sort()
+        self.model.setSort(AccountField.Id.value, Qt.AscendingOrder)
         self.model.select()
 
-    def clear(self):
+    def clearAccounts(self):
         """
-        clear all instances from the model
+        remove all accounts from the database
         """
         self.model.setEditStrategy(QSqlTableModel.OnManualSubmit)
         self.model.removeRows(0, self.model.rowCount())
         self.model.submitAll()
         self.model.setEditStrategy(QSqlTableModel.OnFieldChange)
-        self.sort()
+        self.model.setSort(AccountField.Id.value, Qt.AscendingOrder)
         self.model.select()
-
-    def hideFields(self, fields_to_hide: list()):
-        self.hidden_fields = fields_to_hide
-
-    def sort(self, sort_field=None, sort_order=None):
-        """
-        sort the table.
-        """
-        temp_sort_field = sort_field
-        temp_sort_order = sort_order
-        if sort_field is None:
-            temp_sort_field = self.sort_field
-        if sort_order is None:
-            temp_sort_order = self.sort_order
-        
-        if temp_sort_field in self.field_names:
-            self.sort_field = temp_sort_field
-            self.sort_order = temp_sort_order
-            self.model.setSort(self.field_names.index(self.sort_field), self.sort_order)
-
-    def setRelation(self, field, relation_table, relation_field, displayed_relation_field):
-        """
-        add a foreign key relation to the model
-        """
-        if (
-            self.table_type == TableType.Relational and
-            field in self.field_names and 
-            relation_field in relation_table.field_names and 
-            displayed_relation_field in relation_table.field_names
-            ):
-            self.model.setRelation(
-                self.field_names.index(field),
-                QSqlRelation(relation_table.table_name, relation_field, displayed_relation_field)
-            )
-        else:
-            print("SQL relation attempt failed")
-
-    def setFilter(self, field, value):
-        """
-        add a filter to the model
-        """
-        self.model.setFilter(f"{field}={value}")
-        self.model.select()
-
-    def getRowData(self, query_str):
-        """
-        execute the provided query string to retrieve data for a single row
-        """
-        query = QSqlQuery()
-        query.exec(query_str)
-        query_data = []
-        if query.first():
-            index = 0
-            while query.value(index):
-                query_data.append(query.value(index))
-                index += 1
-        return query_data
-
-
-class CustomTransactionModel(CustomModel):
-    
-    def addFromPayment(self, data):
-        current_date = data.get('StartDate')
-        end_date = data.get('EndDate')
-        frequency = data.get('Frequency')
-        amount = data.get('Amount')
-        transaction_data = {
-            
-        }
-
-        match frequency:
-            case PaymentFrequency.Daily:
-                delta = timedelta(days=1)
-            case PaymentFrequency.Weekly:
-                delta = timedelta(weeks=1)
-            case PaymentFrequency.BiWeekly:
-                delta = timedelta(weeks=2)
-            case PaymentFrequency.Monthly:
-                delta = timedelta(months=1)
-            case PaymentFrequency.BiMonthly:
-                delta = timedelta(months=0.5)
-            case PaymentFrequency.Annually:
-                delta = timedelta(days=365)
-            case PaymentFrequency.SemiAnnually:
-                delta = timedelta(days=365/2)
-            case _:
-                delta = timedelta(days=1)
-
-        while current_date <= end_date:
-            self.add()
-            current_date += delta
